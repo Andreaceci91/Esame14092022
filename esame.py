328d04e2dd8dd2d9d05659a86b4311b985ca1d41
Andrea Ceci <andrea@MacBook-Pro-di-Andrea.local>
2022-09-10 20:01:16 +0200

    Pulita classe CSVTimeSeriesFile

--- esame.py
+++ esame.py
@@ -1,3 +1,4 @@
+
 # Create the class ExamExeption
 class ExamException(Exception):
     pass
@@ -10,8 +11,9 @@ class CSVTimeSeriesFile:
         self.name = name
 
     
-# Create a function get_data
+    # Create a function get_data
     def get_data(self):  
+
             # Try to open the file
             try:
                 file = open(self.name, 'r')
@@ -25,9 +27,6 @@ class CSVTimeSeriesFile:
             #Create a empty list
             data_temp = []
             data = []
-    
-            # Open the file
-            #file = open(self.name, 'r')
 
             for line in file:
                 
@@ -49,39 +48,26 @@ class CSVTimeSeriesFile:
             if len(data_temp) == 0:
                 raise ExamException('Il file non ha valori al suo interno')
 
-            j = 0
-
             # Try to convert data 
             for i in range(len(data_temp)):
                 try:
-                    #print("*****")
-                    # Inserisco float e poi int in quanto se converto direttamente a intero i time_app vengono ignorati
-                    time_app = int(float(data_temp[i][0]))
-                    #print("Tipo:",type(data_temp[i][0]))
-                    #print("***", time_app)
-                    temp_app = float(data_temp[i][1])
-                    #print("Time:", time_app, "Temp:", temp_app)
-                    lista_temp = [time_app, temp_app]
-                    #print("Tipo:", type(lista_temp))
 
-                    # Modifica 1
+                    time_app = int(float(data_temp[i][0]))
+                    temp_app = float(data_temp[i][1])
+
+                    list_temp = [time_app, temp_app]
+
+                    # Check if Value of Epoch are greater than 0
                     if time_app >= 0:
-                        data.append(lista_temp)
+                        data.append(list_temp)
 
                 except:
                     pass
-                    #raise ExamException("Errore nella conversione dei dati")
-
+            
             # Check if epoch are sorted correctly
             for i in range(len(data)-1):
-                j = i+1    
-                while j != len(data):
-                    
-                    if data[i][0] >= data[j][0]:
-                        raise ExamException('Epoch non ordinati correttamente')
-                        #None
+                if data[i][0] >= data[i+1][0]:
+                    raise ExamException('Epoch non ordinati correttamente o duplicati')
 
-                    j += 1
-                    
             # If all check are ok, return data
             return(data)
